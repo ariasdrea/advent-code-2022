8944f9b0b378eb2be6754ceca2db960a6856f105
Andrea Arias <ariasdrea@gmail.com>
2022-12-03 15:39:30 +0100

    🥲

---
+++ 3-rucksack-reorganization/index.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync(__dirname + "/rucksack.txt", "utf8").split("\n");
+
+const cleanUpData = (rucksacks: string[]) => {
+    let repeatedChars: string[][] = [];
+
+    rucksacks.forEach((compartment) => {
+        const firstHalf = compartment.slice(0, compartment.length / 2);
+        const secondHalf = compartment.slice(
+            compartment.length / 2,
+            compartment.length
+        );
+
+        const matches: string[] = [];
+
+        [...firstHalf].forEach((firstHalfChar) => {
+            if (secondHalf.includes(firstHalfChar)) matches.push(firstHalfChar);
+        });
+
+        repeatedChars.push(matches);
+    });
+
+    const cleanedUpArr: string[] = [];
+
+    repeatedChars.forEach((arr) => cleanedUpArr.push(...new Set(arr)));
+
+    return cleanedUpArr;
+};
+
+const calculateSumOfPriorities = (cleanedUpArr: string[]) => {
+    let count = 0;
+
+    cleanedUpArr.forEach((char) => {
+        if (char === char.toLowerCase()) count += parseInt(char, 36) - 9;
+        if (char === char.toUpperCase()) count += parseInt(char, 36) + 17;
+    });
+    return count;
+};
+
+const count = calculateSumOfPriorities(cleanUpData(input));
+console.log({ count });
